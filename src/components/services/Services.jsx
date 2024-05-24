70d3fb8126e58e2502fa38815a81abe43ccbc046
freetimezp <kraftukrnet@ukr.net>
2024-05-24 22:36:56 +0300

    09. js-portfolio-86-robotics. Services component.

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import './services.css';
+import { Link } from 'react-scroll';
+
+import { services } from '../../data';
+import Divider from '../divider/Divider';
 
 const Services = () => {
     return (
-        <div>
-            Services
-        </div>
+        <section id="services" className='blur-effect'>
+            <div className="container">
+                <Divider />
+                <h1 className="title">
+                    Our <span className="text__gradient">Services</span>
+                </h1>
+                <div className="services__container">
+                    {services.map((service, index) => (
+                        <div className="service" key={index}>
+                            <div className="icon__container">{service.icon}</div>
+                            <p className="text__muted">{service.description}</p>
+                            <Link to="contact" className="contact__btn">Contact Us</Link>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
     );
 }
 
